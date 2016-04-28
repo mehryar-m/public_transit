ceb32ef06631f6583001ca9df4c5cc0639ddca1f
Mehryar Maalem <m.maalem@outlook.com>
2016-04-28 13:48:03 -0600

    added regression script

---
+++ scripts/regression.R
@@ -0,0 +1,9 @@
+library(stargazer)
+
+regression.data <- analysis.final[,c("HINCP","atransit")]
+regression.data$logincome <- log(regression.data$HINCP)
+## get rid of nans (produced by negative incomes)
+regression.data <- regression.data[!(is.na(regression.data$logincome)),]
+
+basic.regression <- glm(atransit~logincome, data = regression.data)
+stargazer(basic.regression, title="Basic Regression alternate transit on log(HINC)")
